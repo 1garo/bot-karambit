4d6a87224f72fa8ff49b55e6ecde5451648b82c7
1garo <alevardai@hotmail.com>
2020-08-28 16:36:08 -0300

    call the new functions stop, skip and refactor play to the new pattern

--- services/music-responder.ts
+++ services/music-responder.ts
@@ -13,14 +13,42 @@ export class MusicResponder {
     ){
       this.musicFinder = musicFinder;
     }
-    // TODO: refactor it when your brain came back to work, know its not
-    async play(message: Message, connection: VoiceConnection) {
-       if (this.musicFinder.isPlayMusic(message.content)){
-         console.log('music test! url: ', message.content.replace('!play', ''))
-         let url = message.content.replace('!play', '')
-         connection.play(ytdl(url.trim(), { quality: 'highestaudio', filter: 'audioonly' }));
-         return; 
-       }
-     return Promise.reject();
+    async play<T>(message: Message, 
+      serverQueue: any,
+      queue: Map<any, any>) {
+      const voiceChannel = message.member.voice.channel;
+
+      const {isPlayable, error} = this.musicFinder.
+      isPlayMusic(message.content, message, voiceChannel);
+      if (!isPlayable){
+        console.log(`rejecting the promise: ${isPlayable}`)
+        return Promise.reject(error);
+      }
+      this.musicFinder.execute(message, serverQueue, queue).then(() => {
+        console.log('playing a song!');
+      }).catch(err => {
+        console.log(`error: ${err}`);
+      })
+      return Promise.resolve(); 
     }
+    /*
+      TODO: when skip is used and there is no more music, 
+      throw a message on the chat and don't disconnect the bot
+    */
+    skip(message: Message, queue: any){
+      console.log(`queue: ${queue}`);
+      this.musicFinder.skip(message, queue)
+    }
+    /*
+      TODO: stop are empyting the bot and leaving, 
+      change it to stop the music that is playing  
+    */
+    stop(message: Message, queue: any){
+      console.log(`queue: ${queue}`);
+      this.musicFinder.stop(message, queue);
+    }
+    /*
+      TODO: create a start command that continue to play the music
+      that stop has stopped 
+    */
 }
