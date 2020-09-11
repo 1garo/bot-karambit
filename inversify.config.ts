9d7b0e8d06098ee78bae21471a7a1c914d6a0121
Alexandre Vardai <44412643+1garo@users.noreply.github.com>
2020-09-11 03:36:51 -0300

    remove a debug console.log

--- inversify.config.ts
+++ inversify.config.ts
@@ -8,14 +8,17 @@ import {PingFinder} from "./services/ping-finder";
 import {MusicPlayFinder} from './services/music-finder';
 import {MusicResponder} from './services/music-responder';
 import {TOKEN} from './config.json';
+
 let container = new Container();
-console.log('token = ', process.env.TOKEN);
+
 container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
 container.bind<MessageResponder>(TYPES.MessageResponder).to(MessageResponder).inSingletonScope();
 container.bind<PingFinder>(TYPES.PingFinder).to(PingFinder).inSingletonScope();
 container.bind<MusicPlayFinder>(TYPES.MusicFinder).to(MusicPlayFinder).inSingletonScope();
 container.bind<MusicResponder>(TYPES.MusicResponder).to(MusicResponder).inSingletonScope();
+
 container.bind<Client>(TYPES.Client).toConstantValue(new Client());
 container.bind<string>(TYPES.Token).toConstantValue(TOKEN);
+
 export default container;
 
