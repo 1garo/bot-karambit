8055666e90543158acdfca867e152ca99cd99425
1garo <alevardai@hotmail.com>
2021-03-12 20:55:05 -0300

    feat: rewrite if logic

--- bot.ts
+++ bot.ts
@@ -24,6 +24,14 @@ export class Bot {
     this.pingResponder = pingResponder;
     this.musicResponder = musicResponder;
   }
+  private async action(message, serverQueue, queue) {
+      //const f = this.musicResponder;
+
+      const PLAY = `${message.content.split(' ')[0].slice(1)}`;
+      console.log(`here : ${PLAY}`)
+      PLAY === "play" ? await this.musicResponder[`${message.content.split(' ')[0].slice(1)}`](message, serverQueue, queue) : 
+      await this.musicResponder[`${message.content.split(' ')[0].slice(1)}`](message, serverQueue);
+  }
 
   public async listen(queue: Map<any, any>): Promise<string> {
     this.client.on('message', async (message: Message) => {
@@ -46,20 +54,30 @@ export class Bot {
       //     "You have been disconnect 'cause you are away for too long!"
       //   );
       // }
-      if (message.content.startsWith(`${prefix}play`)) {
-        this.musicResponder.play(message, serverQueue, queue).then(() => {})
-        .catch(err => {
-          err.then((_err: any) => console.log(_err.content));
-        })
-      } else if (message.content.startsWith(`${prefix}skip`)) {
-        this.musicResponder.skip(message, serverQueue);
-      } else if (message.content.startsWith(`${prefix}pause`)) {
-        this.musicResponder.pause(message, serverQueue);
-      } else if (message.content.startsWith(`${prefix}continue`)) {
-        this.musicResponder.continue(message, serverQueue);
-      } else if (message.content.startsWith(`${prefix}exit`)){
-        this.musicResponder.exit(message, serverQueue);
-      }
+
+      const PLAY = `${message.content.split(' ')[0].slice(1)}`;
+      console.log(`here : ${PLAY}`)
+      PLAY === "play" ? await this.musicResponder[`${message.content.split(' ')[0].slice(1)}`](message, serverQueue, queue) : 
+      await this.musicResponder[`${message.content.split(' ')[0].slice(1)}`](message, serverQueue);
+
+      //this.action(message, serverQueue, queue);
+      //this.musicResponder.play(message, serverQueue, queue);
+      //console.log(message.content.startsWith(`${prefix}${message.content.split(' ')[0]}`);
+      //await action[message.content.startsWith(`${prefix}${message.content.split(' ')[0]}`](message, serverQueue, queue);
+    //  if (message.content.startsWith(`${prefix}play`)) {
+    //    this.musicResponder.play(message, serverQueue, queue).then(() => {})
+    //    .catch(err => {
+    //      err.then((_err: any) => console.log(_err.content));
+    //    })
+    //  } else if (message.content.startsWith(`${prefix}skip`)) {
+    //    this.musicResponder.skip(message, serverQueue);
+    //  } else if (message.content.startsWith(`${prefix}pause`)) {
+    //    this.musicResponder.pause(message, serverQueue);
+    //  } else if (message.content.startsWith(`${prefix}continue`)) {
+    //    this.musicResponder.continue(message, serverQueue);
+    //  } else if (message.content.startsWith(`${prefix}exit`)){
+    //    this.musicResponder.exit(message, serverQueue);
+    //  }
     });
     return this.client.login(this.token);
   }
